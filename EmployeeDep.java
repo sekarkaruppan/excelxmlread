a8c11ca7119ebb93e5ccb769dc4f11223d6454c6
sekarkaruppan <45545959+sekarkaruppan@users.noreply.github.com>
2018-12-02 21:09:58 -0500

    Add files via upload

---
+++ EmployeeDep.java
@@ -0,0 +1,27 @@
+package working.xmll;
+
+public class EmployeeDep {
+
+  
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	private Integer id;
+	private String salary;
+	private String dept;
+}
---
+++ EmployeeDepCompare.java
@@ -0,0 +1,47 @@
+package working.xmll;
+
+public class EmployeeDepCompare {
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String location;
+	private String salary;
+	private String dept;
+	
+}
